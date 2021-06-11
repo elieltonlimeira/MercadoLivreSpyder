6bba5b89cb8bab4b990a27851e32fcf486bd2f5a
Elielton Limeira <81534359+elieltonlimeira@users.noreply.github.com>
2021-06-11 01:53:16 -0300

    Update crawl_mlivre.py

--- mlivrescrapy/spiders/crawl_mlivre.py
+++ mlivrescrapy/spiders/crawl_mlivre.py
@@ -7,7 +7,9 @@ class CrawlMlivreSpider(scrapy.Spider):
 
     def parse(self, response):
         item = MlivrescrapyItem()
-        
-        item['nome'] = response.css('.ui-search-item__title::text').getall()
-        item['preco'] = response.css('.ui-search-price--size-medium .ui-search-price__second-line .price-tag-fraction::text').getall()
-        yield item
+        products = response.css('li.ui-search-layout__item')
+       
+        for p in products:
+            item['nome'] = p.css('.ui-search-item__title::text').get()
+            item['preco'] = p.css('.ui-search-price--size-medium .ui-search-price__second-line .price-tag-fraction::text').get()
+            yield item
