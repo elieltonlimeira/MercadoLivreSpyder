36b8ee6fc164bc05fbd844ed4c398c5fa8bd90e9
Elielton Limeira <81534359+elieltonlimeira@users.noreply.github.com>
2021-06-15 23:02:05 -0300

    Adding PrettyTable

--- mlivrescrapy/spiders/crawl_mlivre.py
+++ mlivrescrapy/spiders/crawl_mlivre.py
@@ -1,21 +1,27 @@
 import scrapy
 from ..items import MlivrescrapyItem
+from ..prettytable import PrettyTable
 
 class CrawlMlivreSpider(scrapy.Spider):
     name = 'crawl_mlivre'
-    start_urls = [('https://lista.mercadolivre.com.br/%s' % input(" Qual produto desejas pesquisar? "))]
+    start_urls = [('https://lista.mercadolivre.com.br/%s' % input("Qual produto desejas pesquisar? "))]
 
     def parse(self, response):
         item = MlivrescrapyItem()
+        t = PrettyTable()
+        t.field_names = (['Nome', 'Preço'])
         products = response.css('li.ui-search-layout__item')
+        
        
         for p in products:
             item['nome'] = p.css('.ui-search-item__title::text').get()
             item['preco'] = p.css('.ui-search-price--size-medium .ui-search-price__second-line .price-tag-fraction::text').get()
+            t.add_row([item['nome'],item['preco']])
             yield item
         
         try:
             next_pag = response.css('.andes-pagination__button--next .ui-search-link').attrib['href']
             yield scrapy.Request(self.start_urls[0].replace(self.start_urls[0], next_pag), callback=(self.parse))
         except:
+            print(t)
             quit
